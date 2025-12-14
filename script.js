2dff88e50670ba9502f4055d4d3af39682a6a073
Vinayak Pandey <pandeyvinayak589@gamil.com>
2025-12-14 07:37:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () =>{
+const getWeatherBtn = document.getElementById('get-weather-btn');
+const cityInput = document.getElementById('city-input');
+const weatherInfo = document.getElementById('weather-info');
+const citNameDisplay = document.getElementById('city-name');
+const tempreatureDisplay = document.getElementById("tempreature")
+const discriptionDisplay = document.getElementById('discription')
+const errorMessage = document.getElementById("error-message")
+
+const API_KEY = '1b4be0da30c543e62fec33d18935622b';
+getWeatherBtn.addEventListener('click',async() =>{
+    const city = cityInput.value.trim()
+    if(!city) return
+    //It may throw an error and remember the server and database is in other continent
+    try {
+        const weatherData = await fetchWeatherData(city)
+        displayWeatherData(weatherData)
+    } catch (error) {
+        showError(error)
+    }
+})
+
+async function fetchWeatherData(city){
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+    console.log(typeof response)
+    console.log("RESPONSE", response  )
+    if(!response.ok){
+        throw new Error('Network response was not ok')
+    }
+    const data = await response.json()
+    return data
+}
+function displayWeatherData(data){
+     console.log(data);
+     const {name, main, weather} = data
+    citNameDisplay.textContent = name
+    tempreatureDisplay.textContent = `Temperature: ${main.temp}`
+    discriptionDisplay.textContent = `Weather: ${weather[0].description}`
+    weatherInfo.classList.remove("hidden")     
+    errorMessage.classList.add("hidden")
+}
+
+function showError(message){
+    weatherInfo.classList.add('hidden')
+    errorMessage.classList.remove('hidden')
+}
+    
+});
